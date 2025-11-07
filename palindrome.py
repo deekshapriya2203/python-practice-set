299f150536b0fc47910ea9776a5a13812a9e73da
deekshapriya2203 <deekshapriya2203@gmail.com>
2025-11-07 22:08:33 +0530

    to check whether the string is palindrome

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+def is_palindrome_two_pointer(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+            continue
+        if not s[right].isalnum():
+            right -= 1
+            continue
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
+
+# Example
+print("Is palindrome?", is_palindrome_two_pointer("No lemon, no melon"))
